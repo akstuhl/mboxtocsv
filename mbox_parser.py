c4c2b4a1abe438ed704c4397af0fdccb51a8147c
Andy Stuhl <andykstuhl@gmail.com>
2016-02-03 17:00:17 -0500

    add columns for 'to' and 'body', added header row

--- mbox_parser.py
+++ mbox_parser.py
@@ -2,5 +2,6 @@ import mailbox
 import csv
 
 writer = csv.writer(open("clean_mail.csv", "wb"))
-for message in mailbox.mbox('your_mbox_name'):
-	writer.writerow([message['subject'], message['from'], message['date']])
+for message in mailbox.mbox('Spam.mbox'):
+	writer.writerow(["subject", "from", "to", "date", "body"])
+	writer.writerow([message['subject'], message['from'], message['to'], message['date'], message.get_payload()])
