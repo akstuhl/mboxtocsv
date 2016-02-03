6671b62153c50ad345cf06f62f12dc8b78d84991
Andy Stuhl <andykstuhl@gmail.com>
2016-02-03 18:10:23 -0500

    added parsing for multipart payloads

--- mbox_parser.py
+++ mbox_parser.py
@@ -4,4 +4,7 @@ import csv
 writer = csv.writer(open("clean_mail.csv", "wb"))
 writer.writerow(["subject", "from", "to", "date", "body"])
 for message in mailbox.mbox('Spam.mbox'):
-	writer.writerow([message['subject'], message['from'], message['to'], message['date'], message.get_payload()])
+	payload = message.get_payload()
+	if message.is_multipart():
+		payload = message.get_payload(0).get_payload()
+	writer.writerow([message['subject'], message['from'], message['to'], message['date'], payload])
